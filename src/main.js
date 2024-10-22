830abcc964a5a19f8f6fb434083e1e712ca8ce61
boogie1989 <serhiibuhai.work@gmail.com>
2024-10-22 13:39:29 +0300

    deploy

--- src/main.js
+++ src/main.js
@@ -1,37 +1,52 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-import { searchImages } from "./js/pixabay-api";
-import { showImages } from "./js/render-functions";
+import { searchImages } from './js/pixabay-api';
+import { showImages } from './js/render-functions';
 
-const formElem = document.querySelector("form");
+const formElem = document.querySelector('form');
+const imagesList = document.querySelector('.images-list');
+const loader = document.querySelector('.loader');
 
-formElem.addEventListener("submit", (event) => {
-    event.preventDefault();
+const displayError = message => {
+  iziToast.error({
+    class: 'error-alert',
+    message,
+    messageColor: 'white',
+    position: 'topRight',
+    maxWidth: 432,
+  });
+};
 
-    // Clear the gallery
-    document.querySelector(".images-list").innerHTML = "";
+const handleImageSearch = async event => {
+  event.preventDefault();
+  imagesList.innerHTML = '';
 
-    const form = event.target;
-    const searchQuery = form.elements.query.value;
-    // Show loading message
-    const loader = document.querySelector(".loader");
-    loader.classList.remove("hidden");
-    searchImages(searchQuery)
-        .then((images) => {
-            loader.classList.add("hidden");
-            if (!images.hits.length) {
-                iziToast.error({
-                    class: "error-alert",
-                    message: "Sorry, there are no images matching your search query. Please try again!",
-                    messageColor: "white",
-                    position: "topRight",
-                    maxWidth: 432
-                });
-            }
-            else {
-                showImages(images);
-            }
-        })
-        .catch((error) => console.log(error));
-});
+  const searchQuery = event.target.elements.query.value.trim();
+
+  if (!searchQuery) {
+    displayError('Please enter a search query!');
+    return;
+  }
+
+  loader.classList.remove('hidden');
+
+  try {
+    const images = await searchImages(searchQuery);
+    loader.classList.add('hidden');
+
+    if (!images.hits.length) {
+      displayError(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    } else {
+      showImages(images);
+    }
+  } catch (error) {
+    console.error(error);
+    loader.classList.add('hidden');
+    displayError('An error occurred while fetching images. Please try again.');
+  }
+};
+
+formElem.addEventListener('submit', handleImageSearch);
